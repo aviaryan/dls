d9466c56e0371d0a16753c8c6029d61c4f801967
aviaryan <avi.aryan123@gmail.com>
2016-05-12 23:03:51 +0530

    changed sqlite path to root

--- dls/__init__.py
+++ dls/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../database.sqlite3'
 app.config['SECRET_KEY'] = "random string"
 db = SQLAlchemy(app)
 # Session = sessionmaker(bind=db)
