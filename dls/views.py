2d8a5788a4582cebe329ba54c5c8433347d539c6
aviaryan <avi.aryan123@gmail.com>
2016-05-12 20:44:08 +0530

    added more views

--- dls/views.py
+++ dls/views.py
@@ -1,4 +1,5 @@
 from dls import app
+from flask import url_for, redirect, request
 
 
 @app.route('/')
@@ -9,3 +10,16 @@ def index():
 @app.route('/about/')
 def about():
     return 'This service was created by Avi Aryan as an attempt to learn Flask'
+
+
+@app.route('/<strId>/', methods=['POST', 'GET'])
+def serve(strId):
+    if request.method == 'POST':
+        servType = request.form['type']
+        if servType == 'file':
+            pass
+        elif servType == 'text':
+            pass
+        return redirect(url_for('serve', strId=strId))
+    else:
+        return 'Hello %s' % strId
