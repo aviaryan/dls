1fa07eedec3906f4aba5eeb3645a6f4531d10221
aviaryan <avi.aryan123@gmail.com>
2016-05-18 12:46:02 +0530

    added successful json response test

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,8 +1,9 @@
 from tests import DLSTestCase, unittest
+import json
 
 
 class TestAPI(DLSTestCase):
-    def test_404_response(self):
+    def test_404_json(self):
         resp = self.app.get('/flask.json')
         self.assertEqual(resp.status_code, 404)
 
@@ -10,6 +11,14 @@ class TestAPI(DLSTestCase):
         self.add_text('flask', 'Yohan')
         resp = self.app.get('/flask/text')
         self.assertEqual(resp.data, 'Yohan')
+
+    def test_json(self):
+        self.add_text('flask', 'Yohan')
+        self.upload('flask', 100)
+        page = self.app.get('/flask.json')
+        obj = json.loads(page.data)
+        self.assertEqual(obj['filesize'], 100)
+        self.assertEqual(obj['text'], 'Yohan')
 
 
 if __name__ == '__main__':
